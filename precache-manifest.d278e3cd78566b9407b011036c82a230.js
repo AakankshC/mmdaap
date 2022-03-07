735cc41c97ec24c9f6f9fb49cb75ea44b0e86761
Aakanksh <aakanksh.c@thomsonreuters.com>
2022-03-08 01:53:27 +0530

    Updates

---
+++ precache-manifest.d278e3cd78566b9407b011036c82a230.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab327e2eab88f789a9f840b7b61ed57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f4f9ae1cee5ef0c7e6c",
+    "url": "/static/css/main.66320727.chunk.css"
+  },
+  {
+    "revision": "4a7df2dfb992e50a89ff",
+    "url": "/static/js/2.2d672fa3.chunk.js"
+  },
+  {
+    "revision": "9141e6603b2ba65e9f944cb50ce12050",
+    "url": "/static/js/2.2d672fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bfc641d4dd4284b11e",
+    "url": "/static/js/3.e9523d5c.chunk.js"
+  },
+  {
+    "revision": "1f4f9ae1cee5ef0c7e6c",
+    "url": "/static/js/main.8747a06f.chunk.js"
+  },
+  {
+    "revision": "07e1e4498757b9db2482",
+    "url": "/static/js/runtime-main.d2a647de.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
